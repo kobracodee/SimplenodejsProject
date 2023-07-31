4a8c4b0df4946ba255a40bc36b6cb6c773269460
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-01 00:23:36 +0300

    use express.static middleware

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const { readFile } = require('fs');
-const { request } = require('http');
+const express = require("express");
+const { readFile } = require("fs");
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.static("static"));
+app.get("/", (request, response) => {
+  readFile("./static/index.html", "utf8", (err, html) => {
+    if (err) {
+      response.status(500).send("Out of service");
+    }
 
-    readFile('./home.html', 'utf8', (err, html) => {
-
-        if(err){
-            response.status(500).send('Out of service')
-        }
-
-        response.send(html);
-
-    });
-
+    response.send(html);
+  });
 });
 
-if (typeof document !== 'undefined') {
-    let input = document.querySelector('input-f')
-    document.getElementById("submit-btn").onclick = function(){
-        
-        // badi e3mal shi
-    }
-        
-    
- }
+if (typeof document !== "undefined") {
+  let input = document.querySelector("input-f");
+  document.getElementById("submit-btn").onclick = function () {
+    // badi e3mal shi
+  };
+}
 
-app.listen(process.env.PORT || 3000, () => console.log('Applictation avialable ob http://localhost:3000'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Applictation avialable ob http://localhost:3000")
+);
