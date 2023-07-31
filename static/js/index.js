66734053d946dfdbe783bedfcfae854ee5db3071
kobracodee <112758298+kobracodee@users.noreply.github.com>
2023-08-01 00:51:50 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-if (typeof document !== "undefined"){
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.addEventListener("click", (e) =>
-      console.log("Hi Hussein!\nYou clicked on me!")
-    );
-}
+const submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", (e) =>
+  window.alert("hello")
+);
+
+
